6818bc925611b09bcdee5831253360a9c92482db
Danila Iagupets <d.iagupets@gmail.com>
2022-04-10 14:51:02 +0300

    initial commit

---
+++ src/main/java/com/example/fuzzycommitment/FuzzyCommitmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.fuzzycommitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuzzyCommitmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuzzyCommitmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fuzzycommitment/FuzzyCommitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fuzzycommitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuzzyCommitmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
